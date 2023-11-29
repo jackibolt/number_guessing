359d9b20932b98cded9abdf9ade669d7b91140b4
jackibolt <40141575+jackibolt@users.noreply.github.com>
2023-11-28 23:11:21 -0500

    Update guessing_game.py

--- guessing_game.py
+++ guessing_game.py
@@ -2,49 +2,42 @@
 Python Web Development Techdegree
 Project 1 - Number Guessing Game
 --------------------------------
-
-For this first project we will be using Workspaces. 
-
-NOTE: If you strongly prefer to work locally on your own computer, you can totally do that by clicking: File -> Download Workspace in the file menu after you fork the snapshot of this workspace.
-
 """
 
 import random
 
 
 def start_game():
-    """Psuedo-code Hints
-    
-    When the program starts, we want to:
-    ------------------------------------
-    1. Display an intro/welcome message to the player.
-    2. Store a random number as the answer/solution.
-    3. Continuously prompt the player for a guess.
-      a. If the guess greater than the solution, display to the player "It's lower".
-      b. If the guess is less than the solution, display to the player "It's higher".
-    
-    4. Once the guess is correct, stop looping, inform the user they "Got it"
-         and show how many attempts it took them to get the correct number.
-    5. Let the player know the game is ending, or something that indicates the game is over.
-    
-    ( You can add more features/enhancements if you'd like to. )
-    """
-    # write your code inside this function.
+
+    # welcome
     print('Hi there! Welcome to the number guessing game')
+    # set random number between 1-50
     random_num = random.randrange(1,50)
-    guess = int(input('Guess a number from 1 - 50: '))
+    # count number of guesses
     counter = 0
-    while guess != random_num:
-      if guess > random_num:
-        print("It's lower.")
-        counter +=1
-        guess = int(input('Guess a number from 1 - 50: '))
-      elif guess < random_num:
-        print("It's higher.")
-        counter +=1
-        guess = int(input('Guess a number from 1 - 50: '))
+    # so long as there isn't a break, keep running the function
+    while True:
+      try:
+        # user guess
+        guess = int(input('Guess a number from 1 - 50: '))  
+        if guess <= 0 or guess >= 50:
+          raise Exception(f"{guess} is not between 1 - 50. Please try again.")
+        
+        if guess > random_num:
+          print("It's lower.")
+          counter +=1
+        elif guess < random_num:
+          print("It's higher.")
+          counter +=1
+        elif guess == random_num:
+          counter +=1
+          print(f'''You got it! The random number is {random_num}. It took you {counter} guesses.
+          The game is now over.''')
+          break
+      except ValueError:
+        print('That is not a integar. Please try again.')
+      except Exception as e:
+        print(e)
 
-    print(f'You got it! The random number is {random_num}. It took you {counter} guesses.')
 
-# Kick off the program by calling the start_game function.
 start_game()
